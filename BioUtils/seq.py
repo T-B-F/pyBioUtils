992d46d375677473147c472ea020814499b1a4d5
T-B-F <bitard.feildel.tristan@gmail.com>
2016-12-28 15:02:09 +0900

    msa position convertor

--- BioUtils/seq.py
+++ BioUtils/seq.py
@@ -36,8 +36,29 @@ def compute_offset_pos(seq, pos):
         #print(pos, cnt, k, seq)
     return k
 
+def compute_offset_pos(seq, pos):
+    """ from a MSA sequence, computes the corresponding position in the sequence without gaps
+    
+    Parameters
+    ==========
+    seq : string
+        the sequence from the MSA 
+    pos : int
+        the position to convert
+        
+    Return
+    ======
+    k : int
+        the position in the sequence without gaps
+    """
 
-def compute_msa_offset(seq, start, stop):
+    cnt = 0 
+    for c in seq:
+        if c in msa_characters:
+            cnt += 1
+    return pos - c
+
+def compute_pos_seq2msa(seq, start, stop):
     """ from a start and stop positions of a sequence without gaps, computes the corresponding position in a MSA
     
     Parameters
@@ -58,5 +79,29 @@ def compute_msa_offset(seq, start, stop):
     """
     new_start = compute_offset_pos(seq, start)
     new_stop = compute_offset_pos(seq, stop)
+    return new_start, new_stop
+
+
+def compute_pos_msa2seq(seq, start, stop):
+    """ from a start and stop positions of MSA, computes the corresponding position in the sequence without gaps
+    
+    Parameters
+    ==========
+    seq : string
+        the sequence from the MSA 
+    start : int
+        the position to convert
+    stop : int
+        the position to convert
+        
+    Return
+    ======
+    new_start : int
+        the position in the sequence without gap
+    new_stop : int
+        the position in the sequence without gap
+    """
+    new_start = compute_revoffset_pos(seq, start)
+    new_stop = compute_revoffset_pos(seq, stop)
     return new_start, new_stop
 
