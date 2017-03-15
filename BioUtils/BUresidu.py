d52ec04963240683d82d8b640dcaa21b8e16ae23
T-B-F <tristan.bitard.feildel@gmail.com>
2017-03-15 15:46:16 +0900

    add Atschley2004 vector

--- BioUtils/BUresidu.py
+++ BioUtils/BUresidu.py
@@ -260,3 +260,41 @@ class Frequency(object):
             seq = "".join(np.random.choice(self._aa, p=self._freq) for n in range(size))
             yield seq
             
+class AAnum(object):
+    """ to each AA give a vector of values
+    """
+    __slots__ = ["__data"]
+    def __init__(self):
+        self.__data = {"Atschley2004":{
+            "A": np.array([-0.591, -1.302, -0.733,  1.570, -0.146]),
+            "C": np.array([-1.343,  0.465, -0.862, -1.020, -0.255]),
+            "D": np.array([ 1.050,  0.302, -3.656, -0.259, -3.242]),
+            "E": np.array([ 1.357, -1.453,  1.477,  0.113, -0.837]),
+            "F": np.array([-1.006, -0.590,  1.891, -0.397,  0.412]),
+            "G": np.array([-0.384,  1.652,  1.330,  1.045,  2.064]),
+            "H": np.array([ 0.336, -0.417, -1.673, -1.474, -0.078]),
+            "I": np.array([-1.239, -0.547,  2.131,  0.393,  0.816]),
+            "K": np.array([ 1.831, -0.561,  0.533, -0.277,  1.648]),
+            "L": np.array([-1.019, -0.987, -1.505,  1.266, -0.912]),
+            "M": np.array([-0.663, -1.524,  2.219, -1.005,  1.212]),
+            "N": np.array([ 0.945,  0.828,  1.299, -0.169,  0.933]),
+            "P": np.array([ 0.189,  2.081, -1.628,  0.421, -1.392]),
+            "Q": np.array([ 0.931, -0.179, -3.005, -0.503, -1.853]),
+            "R": np.array([ 1.538, -0.055,  1.502,  0.440,  2.897]),
+            "S": np.array([-0.228,  1.399, -4.760,  0.670, -2.647]),
+            "T": np.array([-0.032,  0.326,  2.213,  0.908,  1.313]),
+            "V": np.array([-1.337, -0.279, -0.544,  1.242, -1.262]),
+            "W": np.array([-0.595,  0.009,  0.672, -2.128, -0.184]),
+            "Y": np.array([ 0.260,  0.830,  3.097, -0.838,  1.512])
+            }
+        }
+        
+    def get(self, name):
+        if not name in self.__data:
+            raise(ValueError, "Unable to find data named {}".format(name))
+        return self.__data[name]
+    
+    def names(self):
+        print(list(self.__data.keys()))
+        
+    
