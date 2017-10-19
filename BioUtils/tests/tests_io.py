d832f8307dffc44449e6390c101c156fdb324555
T-B-F <tristan.bitard.feildel@gmail.com>
2017-10-19 11:57:25 +0200

    add io tests

---
+++ BioUtils/tests/tests_io.py
@@ -0,0 +1,49 @@
+from __future__ import division, absolute_import, print_function
+
+import os, sys
+import numpy as np
+import unittest
+
+from BioUtils import BUio
+
+class TestBUseq(unittest.TestCase):
+
+    def setup(self):
+        self.targets = [("fasta/msa_test1.fa", "fasta/seq_test1.fa", 3, 102),
+                        ("fasta/msa_test2.fa.gz", "fasta/seq_test2.fa", 3, 102)]
+
+                
+
+    def test_msaio(self):
+        """ check msa reading
+        """
+        self.setup()
+        # check full reading
+        for msafilename, seqfilename, nb_seq, len_seq in self.targets:
+            msa_fasta = BUio.read_fastabio(msafilename)
+            proteins = list(msa_fasta.keys())
+            self.assertEqual(nb_seq, len(msa_fasta))
+            self.assertEqual(len(msa_fasta[proteins[0]]), len_seq)
+
+            flat_msa_fasta = BUio.msa2flat(msa_fasta)
+            seq_fasta = BUio.read_fastabio(seqfilename)
+            seqs_flat = [str(flat_msa_fasta[prot].seq) for prot in proteins]
+            seqs = [str(seq_fasta[prot].seq) for prot in proteins]
+            self.assertEqual(seqs, seqs_flat)
+
+        # check iteration loading
+        for msafilename, seqfilename, nb_seq, len_seq in self.targets:
+            seq_fasta = BUio.read_fastabio(msafilename)
+            seqs_msa = list()
+            seqs_seq = list()
+            for record in BUio.read_fastabioit(msafilename):
+                seq = str(record.seq)
+                seqs_msa.append(seq)
+                seqs_seq.append(str(seq_fasta[record.id].seq))
+            self.assertEqual(seqs_msa, seqs_seq)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+        
